9a3b8ea7736fa0ca0a7722c3ec8d305b158d1ab2
Anthony Maisonet <anthony@maisonet.com>
2021-11-01 14:36:08 -0700

    Sort Zero $ Delete Many

--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -26,6 +26,18 @@ inventoryRouter.get("/:inventoryID", (req, res, next) => {
         })
 })
 
+//GET Request - Find iventory items that equal "0"
+inventoryRouter.get("/search/zeroquantity", (req, res, next) => {
+    Inventory.where("quantity").lte(0).exec((err, zeroStock) =>{
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(zeroStock)
+    })
+})
+
+
 //POST Request - ADD ONE
 inventoryRouter.post("/", (req, res, next) => {
     console.log(req.params)
@@ -45,7 +57,22 @@ inventoryRouter.post("/", (req, res, next) => {
 
 //DELETE Request - ONE
 
+//DELETE Request - Delete all inventory tiems whose quantity equals "0"
+inventoryRouter.delete("/deletezero", (req, res, next) => {
+    Inventory.deleteMany(
+        { quantity: 0 },
+        (err, deletedZeroStock) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            // return res.status(200).send(`Sucessfully deleted all items with zero quantity from the database`)
+            return res.status(200).send(`You have deleted ${deletedZeroStock.deletedCount} zero stock items from the database`)
+        })
+})
 
 //SEARCH BY TITLE Request
+
+
 
 module.exports = inventoryRouter
