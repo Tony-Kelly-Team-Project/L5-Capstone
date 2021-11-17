f3a0e4c795f596a42554be37fd8a059cd846dc64
Anthony Maisonet <anthony@maisonet.com>
2021-11-17 08:52:47 -0800

    inventory page changes and some css

--- client/src/App.js
+++ client/src/App.js
@@ -3,47 +3,110 @@ import { Switch, Route } from "react-router-dom"
 import axios from "axios"
 import Home from "./Home"
 import SnapShot from "./SnapShot"
-import Inventory from "./Inventory"
-import InventoryForm from "./InventoryForm"
+import Inventory from "./Inventory.js"
+import InventoryForm from "./InventoryForm.js"
 import Reports from "./Reports"
 import NavBar from "./NavBar"
 import "./index.css"
 
-
-function App() {
+export default function App(props) {
 
   const [inventories, setInventories] = useState([])
+  
+  // created a function to do a "Get" request for inventory items on our server
+  function getInventories () {
+    axios.get("/inventories")
+      .then(res => setInventories(res.data))  // setInventories to be response data which update inventories array
+      .catch(err => console.log(err)) // catch errors
+  }
 
-  const getInventories = () => {
-    axios.get(`/inventories`)
-      .then(res => setInventories(res.data))
+  //function to add item to database from for
+  //function fires when submit - its expects a new inventory as a parameter
+  function addInventory(newInventory){
+    axios.post("/inventories", newInventory) // send the new inventory
+    .then(res => {
+      setInventories(prevInventories => [...prevInventories, res.data])
+      if (res.data.quantity <= 5) {  //checks if quantity is less than or equal to 5
+        alert(`You have less than 5 of ${res.data.title}  left in your inventory`) // sends an alert 
+      }
+    })
+    .catch(err => console.log(err))
+  }
+  // function that allows you to edit and item in the inventory
+  function editInventory(updates, inventoryId){
+    axios.put(`/inventories/${inventoryId}`, updates)
+      .then(res => {
+        setInventories(prevInventories => prevInventories.map(inventory => inventory._id !==inventoryId ? inventory : res.data))
+        if (res.data.quantity <= 5) {  //checks if quantity is less than or equal to 5
+          alert(`You have less than 5 of ${res.data.title}  left in your inventory`) // sends an alert 
+        }
+      })
       .catch(err => console.log(err))
+  }
+  // function that allows you to delete individual items 
+  function deleteInventory(inventoryId){
+    axios.delete(`/inventories/${inventoryId}`)
+    .then(res => {
+      setInventories(prevInventories => prevInventories.filter(inventory => inventory._id !== inventoryId))
+      
+    })
+    .catch(err => console.log(err))
   }
 
   //useEffect
   useEffect(() => {
     console.log("useEffect triggered")
-    getInventories()
+    getInventories ()
   }, [])
-
-
-
+  
   return (
 
-    <div>
+    <div >
 
       <NavBar />
-
-      <Switch>
-
+      
+      <Switch >
         <Route exact path="/">
           <Home />
           <SnapShot />
         </Route>
 
         <Route path="/inventory">
-          <Inventory />
-          <InventoryForm />
+        <h5></h5>
+        <InventoryForm
+            submit={ addInventory }
+            btnText="Add Ebay item"
+            />
+            <div className="app-container">
+          <table>
+            <thead>
+              <h5>eBay Inventory</h5>
+              <tr>
+                <th>Title</th>
+                <th>Condition</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Location</th>
+                <th>SKU</th>
+                <th>Categories</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+          </table>
+          </div>
+
+
+          {inventories.map((inventory, index) =>
+              <Inventory
+                {...inventory}
+                key={inventory._id}
+                index={index}
+                editInventory={ editInventory }
+                deleteInventory = { deleteInventory }
+
+              />
+            )
+            }
         </Route>
 
         <Route path="/reports">
@@ -52,15 +115,16 @@ function App() {
               {...inventory}
               key={inventory._id}
               index={index}
+              
             />
           )
           }
         </Route>
-
+        
       </Switch>
 
     </div>
   );
 }
 
-export default App;
+// export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './App';
+import "./index.css"
 
 
 ReactDOM.render(
-    <Router>
+    
+    <Router >
+        <div className = "pageStyle">
         <App />
+        </div>
     </Router>,
     document.getElementById('root')
 );
